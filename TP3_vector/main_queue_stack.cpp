aeb924292329fe5da84f2396e958846e55ed0576
Peter <lin.peter@outlook.fr>
2023-12-14 13:59:10 +0100

    palindrome check

---
+++ TP3_vector/main_queue_stack.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stack>
+#include <cctype>
+
+using namespace std;
+
+bool isPalindrome(const string& input) {
+    stack<char> s;
+    for (char c : input) {
+        s.push(c);
+    }
+    for (char c : input) {
+        if (c != s.top()) {
+            return false;
+        }
+        s.pop();
+    }
+    return true;
+}
+
+int main () {
+    cout << "test isPalindrome" << endl ;
+    cout << "Is 'racecar' a palindrome ?"
+    << isPalindrome( "racecar" ) << endl ;
+    cout << " Is ' hello ' a palindrome ? "
+    << isPalindrome("hello" ) << endl ;
+    return 0 ;
+}
